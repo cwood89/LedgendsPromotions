3be4374d55e32f93b5b31ac771908bbc729fb6a9
woodtop <cwood89@mac.com>
2019-08-29 16:13:02 -0400

    finished heading

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,9 @@ class Header extends Component {
     return (
       <header>
         <h1>Legends Promotions</h1>
+        <h2>Event Services</h2>
+        <p>Legends Promotions proudly serves the Charelston, SC area. </p>
+        <p>Services include photography, karaoke, and many others.</p>
       </header>
     );
   }
